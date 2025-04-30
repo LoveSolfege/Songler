c67ef8d1831d122e7d839affecf8abe1fdb5c161
lovesolfege <example@mail.org>
2025-04-30 22:21:29 +0400

    horrible find by ID implementation

--- SonglerAPI/Endpoints/General/EndpointBase.cs
+++ SonglerAPI/Endpoints/General/EndpointBase.cs
@@ -14,18 +14,41 @@ public static class EndpointBase
 		Func<SongContext, DbSet<TEntity>> getDbSet)
 		where TEntity : class
 	{
+		//create endpoint group
 		var group = app.MapGroup(route).WithTags(tag);
 		
 		//GET all entries
 		group.MapGet("/", async (SongContext ctx, IMapper mapper) =>
 		{
 			var entities = await getDbSet(ctx)
-				.AsNoTracking().
-				ToListAsync();
+				.AsNoTracking()
+				.ToListAsync();
 			
 			return Results.Ok(mapper.Map<List<TResponseDto>>(entities));
 		});
+
 		
+		//GET enty by ID
+		group.MapGet("/{id:int}", async (int id, SongContext ctx, IMapper mapper) =>
+		{
+			var entity = await getDbSet(ctx)
+				.FindAsync(id);
+			
+			return entity is null ? Results.NotFound() : Results.Ok(mapper.Map<TResponseDto>(entity));
+		});
+		
+		/*
+		group.MapPost("/new", async (TCreateDto dto, SongContext ctx, IMapper mapper) =>
+		{
+			var entity = mapper.Map<TEntity>(dto);
+			ctx.Add(entity);
+			await ctx.SaveChangesAsync();
+			
+			var responseDto = mapper.Map<TResponseDto>(entity);
+			
+			return Results.Created($"{route}/{entity}", responseDto);
+		});
+		*/
 		return group;
 	}
 }
