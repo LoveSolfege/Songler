fc1be558f5b5246d96296c2b7ce3df9e9b4349d8
lovesolfege <example@mail.org>
2025-05-11 08:13:22 +0400

    added tests project for APIs

---
+++ SonglerAPI.Tests/EndpointTests/Artists.cs
@@ -0,0 +1,42 @@
+using Xunit;
+using SonglerAPI;
+namespace SonglerAPI.Tests.EndpointTests;
+
+public class Artists
+{
+	[Fact]
+	public void ArtistGetAll()
+	{
+		
+	}
+
+	[Fact]
+	public void ArtistGetById()
+	{
+		
+	}
+
+	[Fact]
+	public void ArtistCreate()
+	{
+		
+	}
+
+	[Fact]
+	public void ArtistUpdate()
+	{
+		
+	}
+
+	[Fact]
+	public void ArtistDelete()
+	{
+		
+	}
+
+	[Fact]
+	public void ArtistRestore()
+	{
+		
+	}
+}
---
+++ SonglerAPI.Tests/EndpointTests/Grades.cs
@@ -0,0 +1,6 @@
+namespace SonglerAPI.Tests.EndpointTests;
+
+public class Grades
+{
+	
+}
