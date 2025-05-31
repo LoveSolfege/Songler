227c651fe146a7cbb98165734b809517298c3f9b
LoveSolfege <contact@lovesolfege.cc>
2025-06-01 00:07:39 +0400

    made AssemblyReference.cs static

--- src/Application/AssemblyReference.cs
+++ src/Application/AssemblyReference.cs
@@ -2,7 +2,7 @@ using System.Reflection;
 
 namespace Application;
 
-public class AssemblyReference
+public static class AssemblyReference
 {
     public static readonly Assembly Assembly = typeof(AssemblyReference).Assembly;
 }
