83c4049924d4a4305a53cb37a3d8b87ccce86830
LoveSolfege <contact@lovesolfege.cc>
2025-06-01 01:45:45 +0400

    added MediatR to the API project

--- src/API/Program.cs
+++ src/API/Program.cs
@@ -1,4 +1,9 @@
+using MediatR;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddMediatR(Application.AssemblyReference.Assembly);
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
