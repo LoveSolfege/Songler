9895da4876b6007432a235abf9f8f3f85308bc89
LoveSolfege <contact@lovesolfege.cc>
2025-06-02 01:45:25 +0400

    added basic middleware to program.cs

--- src/API/Program.cs
+++ src/API/Program.cs
@@ -4,13 +4,27 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddProblemDetails();
+
+//Ignore loops in EF entities
+//builder.Services.AddControllers()
+//    .AddJsonOptions(options => 
+//        options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
+
+
 builder.Services.AddMediatR(Application.AssemblyReference.Assembly);
 
 builder.Services.AddDbContext<SongDbContext>(options => 
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-
 var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler();
+}
+app.UseStatusCodePages();
 
 app.MapGet("/", () => "Hello World!");
 
