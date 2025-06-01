23b96e7fe2a9ed3b99bf9909beb6c7ca101f8e1f
LoveSolfege <contact@lovesolfege.cc>
2025-06-02 01:37:16 +0400

    added ef core to API project

--- src/API/Program.cs
+++ src/API/Program.cs
@@ -1,8 +1,14 @@
+using Infrastructure.Persistence;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddMediatR(Application.AssemblyReference.Assembly);
+
+builder.Services.AddDbContext<SongDbContext>(options => 
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 var app = builder.Build();
 
