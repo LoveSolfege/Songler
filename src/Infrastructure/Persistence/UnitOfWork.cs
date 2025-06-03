d0b008f775bf54a316262c8a4d3caa1b94ae332f
LoveSolfege <contact@lovesolfege.cc>
2025-06-04 00:51:22 +0400

    changed requirements from solid implementations to interfaces

--- src/Infrastructure/Persistence/UnitOfWork.cs
+++ src/Infrastructure/Persistence/UnitOfWork.cs
@@ -1,23 +1,22 @@
 using Domain.Entities;
 using Domain.Interfaces;
-using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Persistence;
 
 public class UnitOfWork : IUnitOfWork
 {
-    private readonly DbContext _context;
+    private readonly SongDbContext _context;
     public IRepository<Artist> Artists { get; }
     public IRepository<Album> Albums { get; }
     public IRepository<Song> Songs { get; }
     public IRepository<Grade> Grades { get; }
 
     public UnitOfWork(
-        DbContext context,
-        Repository<Artist> artists,
-        Repository<Album> albums,
-        Repository<Song> songs,
-        Repository<Grade> grades)
+        SongDbContext context,
+        IRepository<Artist> artists,
+        IRepository<Album> albums,
+        IRepository<Song> songs,
+        IRepository<Grade> grades)
     {
         _context = context;
         Artists = artists;
